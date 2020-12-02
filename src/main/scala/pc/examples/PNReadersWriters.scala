d2152c74012bfeb7d81fc7439f97c0369f220c51
Ylenia Battistini <ylenia.battistini@studio.unibo.it>
2020-12-02 11:05:30 +0100

    safety API

--- src/main/scala/pc/examples/PNReadersWriters.scala
+++ src/main/scala/pc/examples/PNReadersWriters.scala
@@ -26,5 +26,25 @@ object PNReadersWriters extends App {
   ))
 
   // example usage
-  println(readersWritersSystem().paths(MSet(IDLE,LOCK),10).toList.mkString("\n"))
+ // println(readersWritersSystem().paths(MSet(IDLE,LOCK),10).toList.mkString("\n"))
+
+  //small API
+  def verifyAPI(mSet: List[MSet[Place]]): Boolean ={
+    //use other method
+    safetyCheck(10)(mSet)
+  }
+
+  def safetyCheck(depth: Int)(condition: List[MSet[Place]]): Boolean ={
+    val list = readersWritersSystem().paths(MSet(IDLE, IDLE, LOCK),depth).toList
+    println(list)
+    list.forall(m => m.forall(e => !condition.exists(c => e.matches(c))))
+  }
+
+  //safetyCheck(10)(verifyAPI(List(MSet(READING, WRITING),MSet(WRITING, WRITING))))
+  /*val list = readersWritersSystem().paths(MSet(IDLE, IDLE, LOCK),5).toList
+  println(list)
+  val prova = list.forall(m => m.forall(e => !(e.matches(MSet(WRITING, WRITING)) || e.matches(MSet(WRITING, READING)))))
+  println(prova)
+  */
+  print(verifyAPI(List(MSet(IDLE, IDLE), MSet(WRITING, IDLE))))
 }
